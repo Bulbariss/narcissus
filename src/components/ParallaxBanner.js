c6d60afcedae21b8e9ef9ac6462391f1f291de41
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-10-16 18:21:23 +0300

    fix2

--- src/components/ParallaxBanner.js
+++ src/components/ParallaxBanner.js
@@ -4,7 +4,7 @@ const ParallaxBanner = ({ image }) => {
   return (
     <div className="relative flex items-center justify-center w-screen h-screen parallax">
       <div
-        className="w-screen h-screen bg-center bg-no-repeat bg-cover parallax-bg"
+        className="absolute top-0 left-0 w-full h-full bg-center bg-no-repeat bg-cover parallax-bg"
         style={{ backgroundImage: `url(${image})` }}
       />
     </div>
