7bc693489b36c8642b260508de124a1d4b5ac491
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-10-17 14:04:50 +0300

    fix height issues

--- src/components/ParallaxBanner.js
+++ src/components/ParallaxBanner.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const ParallaxBanner = ({ image }) => {
   return (
-    <div className="relative flex items-center justify-center w-screen h-screen parallax">
+    <div className="relative w-screen h-screen parallax">
       <div
-        className="absolute top-0 left-0 w-full h-full bg-center bg-no-repeat bg-cover parallax-bg"
-        style={{ backgroundImage: `url(${image})` }}
+        className="absolute top-0 left-0 w-screen h-screen bg-center bg-no-repeat bg-cover parallax-bg"
+        style={{ backgroundImage: `url(${image})`, zIndex: "-1" }}
       />
     </div>
   );
