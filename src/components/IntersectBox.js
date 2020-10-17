3a59132583cce4047bc1afb35ed010f56449bfd1
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-10-17 22:39:24 +0300

    performance improvements

--- src/components/IntersectBox.js
+++ src/components/IntersectBox.js
@@ -1,54 +1,63 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useInViewport, useEventListener } from "ahooks";
 
 const IntersectBox = ({ image }) => {
   const ref = useRef();
   const img = useRef();
-  const clientHeight = useRef(
+
+  let latestKnownScrollY = 0;
+  let ticking = false;
+
+  let [clientHeight, setClientHeight] = useState(
     typeof window !== `undefined` && window.screen.height
   );
   const inViewPort = useInViewport(ref);
-  let myReq = useRef();
+
+  function update() {
+    ticking = false;
+    img.current.style.transform = `translateY(${latestKnownScrollY}px)`;
+  }
+
+  function onScroll() {
+    latestKnownScrollY = -0.5 * ref.current.getBoundingClientRect().y;
+    requestTick();
+  }
+
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+    }
+    ticking = true;
+  }
 
   const onResize = () => {
     if (clientHeight.current !== window.screen.height) {
-      clientHeight.current = window.screen.height;
-      img.current.style.transform = `translateY(${
-        -0.5 * ref.current.getBoundingClientRect().y
-      }px)`;
+      setClientHeight(window.screen.height);
+      img.current.style.transform = `translateY(${latestKnownScrollY}px)`;
     }
   };
-  function go() {
-    if (inViewPort) {
-      img.current.style.transform = `translateY(${
-        -0.5 * ref.current.getBoundingClientRect().y
-      }px)`;
-      myReq.current = window.requestAnimationFrame(go);
-    } else {
-      myReq.current = null;
-    }
-  }
 
   useEventListener("resize", onResize);
   useEffect(() => {
     if (inViewPort) {
-      myReq.current = window.requestAnimationFrame(go);
+      window.addEventListener("scroll", onScroll, false);
     } else {
-      cancelAnimationFrame(myReq.current);
+      window.removeEventListener("scroll", onScroll, false);
     }
     return () => {
-      cancelAnimationFrame(myReq.current);
+      window.removeEventListener("scroll", onScroll, false);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inViewPort]);
   return (
     <div
-      className="relative h-screen text-6xl bg-white"
+      className="relative h-screen"
       ref={ref}
-      style={{ zIndex: "-1", transform: "translateZ(0)" }}
+      style={{ zIndex: "-1", transform: "translateZ(0)", height: clientHeight }}
     >
       <div
         ref={img}
-        className="absolute w-full h-screen bg-center bg-no-repeat bg-cover test"
+        className="absolute w-full h-full bg-center bg-no-repeat bg-cover test"
         style={{
           backgroundImage: `url(${image})`,
           backfaceVisibility: "hidden",
